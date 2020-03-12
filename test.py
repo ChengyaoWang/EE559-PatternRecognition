03438598deed9feb7d4cb68b39fe205b8bfcd74b
Chengyao Wang <chengyaowang@chengyaos-mbp.home>
2020-03-11 17:08:23 -0700

    SourceCode Name Changes & Report Upload

--- test.py
+++
@@ -1,7 +0,0 @@
-import numpy 
-print "This is a test file \nPerforming a natural number addition as a test program"
-sum=0
-for i in range(0,50):
-	sum+=i
-print "The result of this routine is",sum
-print "Test ends here"
